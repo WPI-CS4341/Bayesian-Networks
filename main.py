a97a9628fb14dc527b4eab293704d62e60fcd35e
Harry Liu <byliuyang11@gmail.com>
2016-03-01 18:40:06 -0500

    Added data

--- main.py
+++ main.py
@@ -44,11 +44,9 @@ def weighted_sample(bn, e):
     assignments = {}
     i = 0
     w = 1
-    v = None
     # Iterate through the nodes of the Bayesian network
     for node in ts:
         r = random.uniform(0, 1)
-        p = r
         # If the node has parents
         if len(node.parents()) > 0:
             # Calculate the probability given parent values
@@ -174,112 +172,119 @@ def assign_status(filename, network):
         return False
 
 
+def convergence_test():
+    """Run sampling method several times until it converge"""
+
+def generate_cond_prob_table():
+    """Generate conditional probability table"""
+    return []
+
 def main():
-    # Read command line arguments
-    args = sys.argv[1:]
-    # More than 1 argument supplied
-    if len(args) > 2:
-        # Get data filename
-        network_file = args[0]
-        query_file = args[1]
-        num_samples = args[2]
-        # Read each line of node file
-        if os.path.isfile(network_file):
-            with open(network_file, "r") as infile:
-                nodes = {}  # Node dictionary
-                index = 0
-                for line in infile:
-                    # Strip the line of newlines and tabs
-                    line = line.strip()
-                    # Get the node name
-                    name = line[:line.index(":")]
-                    # Get the node list
-                    b1 = line.index("[") + 1
-                    b2 = line.index("]", b1)
-                    g1 = line[b1:b2]
-                    parents = g1.split(" ") if len(g1) > 0 else []
-                    # Get the conditional probability table values
-                    b3 = line.index("[", b2) + 1
-                    b4 = line.index("]", b3)
-                    g2 = line[b3:b4]
-                    cptv = g2.split(" ") if len(g2) > 0 else []
-                    # Convert the CPTVs into a table
-                    table = []
-                    for i in xrange(0, len(cptv)):
-                        row = []
-                        for j in xrange(0, len(parents)):
-                            # Create a binary mask for isolating the boolean
-                            # value
-                            mask = pow(2, j)
-                            # Apply the mask to the index of the CPTV
-                            result = i & mask
-                            # Resolve the mask result to a 1 (true) or 0
-                            # (false) and add it to the row
-                            row.append(bool(result))
-                        # Add the CPT value to the end of the row
-                        row.append(float(cptv[i]))
-                        table.append(row)
-                    # Write info to node
-                    this_node = nodes[name] if name in nodes else Node(name)
-                    this_node.index = index
-                    this_node.addCPT(table)
-                    # Generate parent nodes if they don't exist
-                    for parent in parents:
-                        parent_node = nodes[
-                            parent] if parent in nodes else Node(parent)
-                        # Add current node to the children of parent nodes
-                        parent_node.addChild(this_node)
-                        nodes[parent] = parent_node
-                    nodes[name] = this_node
-                    index += 1
+    for i in xrange(10):
+        # Read command line arguments
+        args = sys.argv[1:]
+        # More than 1 argument supplied
+        if len(args) > 2:
+            # Get data filename
+            network_file = args[0]
+            query_file = args[1]
+            num_samples = args[2]
+            # Read each line of node file
+            if os.path.isfile(network_file):
+                with open(network_file, "r") as infile:
+                    nodes = {}  # Node dictionary
+                    index = 0
+                    for line in infile:
+                        # Strip the line of newlines and tabs
+                        line = line.strip()
+                        # Get the node name
+                        name = line[:line.index(":")]
+                        # Get the node list
+                        b1 = line.index("[") + 1
+                        b2 = line.index("]", b1)
+                        g1 = line[b1:b2]
+                        parents = g1.split(" ") if len(g1) > 0 else []
+                        # Get the conditional probability table values
+                        b3 = line.index("[", b2) + 1
+                        b4 = line.index("]", b3)
+                        g2 = line[b3:b4]
+                        cptv = g2.split(" ") if len(g2) > 0 else []
+                        # Convert the CPTVs into a table
+                        table = []
+                        for i in xrange(0, len(cptv)):
+                            row = []
+                            for j in xrange(0, len(parents)):
+                                # Create a binary mask for isolating the boolean
+                                # value
+                                mask = pow(2, j)
+                                # Apply the mask to the index of the CPTV
+                                result = i & mask
+                                # Resolve the mask result to a 1 (true) or 0
+                                # (false) and add it to the row
+                                row.append(bool(result))
+                            # Add the CPT value to the end of the row
+                            row.append(float(cptv[i]))
+                            table.append(row)
+                        # Write info to node
+                        this_node = nodes[name] if name in nodes else Node(name)
+                        this_node.index = index
+                        this_node.addCPT(table)
+                        # Generate parent nodes if they don't exist
+                        for parent in parents:
+                            parent_node = nodes[
+                                parent] if parent in nodes else Node(parent)
+                            # Add current node to the children of parent nodes
+                            parent_node.addChild(this_node)
+                            nodes[parent] = parent_node
+                        nodes[name] = this_node
+                        index += 1
 
-                # Convert the dictionary into a sorted array for processing
-                network = nx.DiGraph()
-                for n in nodes:
-                    node = nodes[n]
-                    parents = node.parents()
-                    network.add_node(node.name, obj=node)
-                    for p in parents:
-                        network.add_edge(p.name, node.name)
-                # Assign statuses in place
-                assign_status(query_file, network)
+                    # Convert the dictionary into a sorted array for processing
+                    network = nx.DiGraph()
+                    for n in nodes:
+                        node = nodes[n]
+                        parents = node.parents()
+                        network.add_node(node.name, obj=node)
+                        for p in parents:
+                            network.add_edge(p.name, node.name)
+                    # Assign statuses in place
+                    assign_status(query_file, network)
 
-                if DEBUG:
-                    # Print out nodes
-                    for node in nodes:
-                        n = nodes[node]
-                        cpt_headers = []
-                        print "\nName: " + n.name
-                        print "Status " + n.status
-                        print "Children: " + ",".join(map(str, n.children()))
-                        print "Parents: " + ",".join(map(str, n.parents()))
-                        [cpt_headers.append(x.name)
-                         for x in n.parents()]
-                        cpt_headers.append("P(n)")
-                        print "CPT: \n\n" + tabulate(n.cpt, headers=cpt_headers) + "\n\n========================="
+                    if DEBUG:
+                        # Print out nodes
+                        for node in nodes:
+                            n = nodes[node]
+                            cpt_headers = []
+                            print "\nName: " + n.name
+                            print "Status " + n.status
+                            print "Children: " + ",".join(map(str, n.children()))
+                            print "Parents: " + ",".join(map(str, n.parents()))
+                            [cpt_headers.append(x.name)
+                             for x in n.parents()]
+                            cpt_headers.append("P(n)")
+                            print "CPT: \n\n" + tabulate(n.cpt, headers=cpt_headers) + "\n\n========================="
 
-                qe = get_query_evidence(network)
-                query = "P(" + qe['X'] + "|" + ','.join(qe['e']) + ")"
+                    qe = get_query_evidence(network)
+                    query = "P(" + qe['X'] + "|" + ','.join(qe['e']) + ")"
 
-                # Run rejection sampling
-                rs = rejection_sampling(
-                    qe['X'], qe['e'], network, int(num_samples))
-                rs = '-' if not rs else rs[True]
+                    # Run rejection sampling
+                    rs = rejection_sampling(
+                        qe['X'], qe['e'], network, int(num_samples))
+                    rs = '-' if not rs else rs[True]
 
-                # Run liklihood weighting
-                l, w = weighted_sample(network, qe['e'])
-                lw = likelihood_weighting(qe['X'], qe['e'], network, int(num_samples))
-                lw = '-' if not lw else lw[True]
+                    # Run liklihood weighting
+                    lw = likelihood_weighting(qe['X'], qe['e'], network, int(num_samples))
+                    lw = '-' if not lw else lw[True]
 
-                # Print result table
-                print '\n' + tabulate([[query, rs, lw]], ["Query", "RS", "LW"])
+                    # Print result table
+                    print '\n' + tabulate([[query, rs, lw]], ["Query", "RS", "LW"])
 
+            else:
+                # Throw error when cannot open file
+                print("Input file does not exist.")
         else:
-            # Throw error when cannot open file
-            print("Input file does not exist.")
-    else:
-        # Show usage when not providing enough argument
-        print("Usage: python main.py <network_file> <query_file> <num_samples>")
+            # Show usage when not providing enough argument
+            print("Usage: python main.py <network_file> <query_file> <num_samples>")
 
 
 if __name__ == '__main__':
